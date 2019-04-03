daf6830563a2ac31baa62a9d7c3435cd4ccb7657
usuariogit <correo@correo.com>
2019-04-02 23:37:23 -0500

    Validation of inputs

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { MoneyService } from '../money.service';
 import { environment } from '../../environments/environment';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+//import { CustomValidator } from './shared/validation';
 
 @Component({
   selector: 'app-main',
@@ -9,31 +11,52 @@ import { environment } from '../../environments/environment';
 })
 export class MainComponent implements OnInit {
 
-  constructor(private moneyService: MoneyService) { }
+  constructor(private moneyService: MoneyService, private fb: FormBuilder) { }
   
   dollar: number;
   euro: number;
   objectApi: any;
   interval: any;
+  formName: FormGroup;
 
   convertDollarToEuro() {
     this.moneyService.convertDollarToEuro().subscribe(res => {
       this.objectApi = res;
-      this.euro=this.objectApi.rates.CAD*this.dollar;
+      this.euro=this.formatNumber(this.objectApi.rates.CAD*this.dollar);
     });
   }
   async convertDollarToEuroAsync() {
     this.objectApi=await this.moneyService.convertDollarToEuroAsync();
-    this.euro=this.objectApi.rates.CAD*this.dollar;
+    this.euro=this.formatNumber(this.objectApi.rates.CAD*this.dollar);
   }
 
   ngOnInit() {
     this.dollar = 0;
     this.euro = 0;
-    //Call each 10 minutes service to refresh values
+    //Call each 1 minutes service to refresh values
     this.interval = setInterval(() => {
         this.convertDollarToEuro();
     }, (environment.timerRequest * 60)*1000);
+    //Form validator
+    this.formName = this.fb.group({
+      numberInput: ['', [Validators.required, Validators.pattern(/^[0-9]+(\.[0-9]{1,4})?$/)]],
+      numberInputEuro: ['']
+     });
   }
+
+  // validateNumber(e: any) {
+  //     let input = String.fromCharCode(e.charCode);
+  //     const reg = /^\d+(\,\d{0,4})? *?$/;
+    
+  //     if (!reg.test(input)) {
+  //       e.preventDefault();
+  //     }
+  // }
+
+  formatNumber(number) {
+      return number.toString().replace(/(\.\d+)|\B(?=(\d{3})+(?!\d))/g, function(m,g1){
+          return g1 || ","
+      });
+  };
 
 }
